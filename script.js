74d1b957469b7a588e01f27ca9420ec9e138162a
Anshumendra singh <94458751+Anshumendra@users.noreply.github.com>
2026-02-13 20:35:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ playSong2Btn.addEventListener("click", () => {
   playSong(song2, "Chandi Jaisa Rang");
 });
 
-// ----- NEW LETTER TEXT -----
+// ----- YOUR NEW LETTER TEXT -----
 const letterText = `Mary ❤️ There is a world beyond those mountains, where the river is the river and the river is the river. There are houses between the mountains and the sky is below, Mary must be hiding under your feet and fireflies will be twinkling like earrings. Mary don't ask about that world, there fireflies have taken the place of stars. People in that world have tied more threads of love than your sons, whose mention is still on the lips bubbling in the grave. No one is lonely in that world, there is no employment except love Mary ❤️. Mary ❤️ No one ages there, the sun never goes to hide and the moon never looks above your feet. The matter of that world is different, there the lamps burn in strong winds and the boat moves on the mountains. Mary ❤️ There the seasons are pink, the leaves of the trees never turn yellow. It rains there too Mary when someone hums a song in love. The eagles, crows and vultures there don't eat meat, they bite the drops of dew, that land is so pure and clean that the souls coming out of the graves hug those who love them. One day when we will get tired of this world, we will call the angels there. Mary, in that world everything said to you will be a promise and nothing will be incomplete for us. We will sing our songs there, you make tunes there.
 
 Mary there the meaning of life will be love`;
